75313b6c87e78990bf51de32410e40056ad01c94
Dyuti-Nandi <dyutinandi6@gmail.com>
2025-09-06 19:51:07 +0530

    updated recursion

--- Recursion/src/ArrayQuestions/Find.java
+++ Recursion/src/ArrayQuestions/Find.java
@@ -7,16 +7,17 @@ public class Find {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 4, 5};
         int target = 4;
-        System.out.println(find(arr, target, 0));
-        System.out.println(findIndex(arr, target, 0));
-        System.out.println(findIndexFromLast(arr, target, arr.length-1));
-        findAllIndex(arr, target, 0);
-        System.out.println(list);
-//        ArrayList <Integer> list = new ArrayList<>();
-//        System.out.println(findMulIndex(arr, target, 0, list));
-//        System.out.println(findMulIndex(arr, target, 0, new ArrayList<>()));
-        ArrayList<Integer> ans = findMulIndex(arr, target, 0, new ArrayList<>());
-        System.out.println(ans);
+//        System.out.println(find(arr, target, 0));
+//        System.out.println(findIndex(arr, target, 0));
+//        System.out.println(findIndexFromLast(arr, target, arr.length-1));
+//        findAllIndex(arr, target, 0);
+//        System.out.println(list);
+////        ArrayList <Integer> list = new ArrayList<>();
+////        System.out.println(findMulIndex(arr, target, 0, list));
+////        System.out.println(findMulIndex(arr, target, 0, new ArrayList<>()));
+//        ArrayList<Integer> ans = findMulIndex(arr, target, 0, new ArrayList<>());
+//        System.out.println(ans);
+        System.out.println(findMulIndex2(arr, target, 0));
     }
 
     public static boolean find(int[] arr, int target, int index){
@@ -69,5 +70,19 @@ public class Find {
         }
 
         return findMulIndex(arr, target, index+1, list);
+    }
+
+    public static ArrayList<Integer> findMulIndex2(int[] arr, int target, int index){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        }
+
+        ArrayList<Integer> ansFromBelowCalls = findMulIndex2(arr, target, index+1);
+        list.addAll(ansFromBelowCalls);
+        return list;
     }
 }
